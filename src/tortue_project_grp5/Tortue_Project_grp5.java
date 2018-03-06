f61b271b629a52eb64e43ea5b2f30d6d79a40826
Adele.R <adelerichard@free.fr>
2018-03-06 14:31:54 +0100

    "rajout connection"

--- src/tortue_project_grp5/Tortue_Project_grp5.java
+++ src/tortue_project_grp5/Tortue_Project_grp5.java
@@ -5,12 +5,38 @@
  */
 package tortue_project_grp5;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Adèlounette
  */
 public class Tortue_Project_grp5 {
 
+       public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/db/chinook.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
